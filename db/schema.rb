8e8bb35c7a6b21614a2b1e2481de4766d070c40d
Julie Gastellu <julie.gastellu@orange.fr>
2022-03-23 19:08:45 +0100

    putin de version

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_03_23_135707) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "hotels", force: :cascade do |t|
     t.string "name"
     t.string "address"
